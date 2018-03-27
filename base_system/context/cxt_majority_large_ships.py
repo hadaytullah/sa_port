88c7313bc8a065e3ed34c556e0b733812dfe160b
Hadaytullah <hadaytul@lm7-cs-21.local>
2018-03-27 13:39:39 +0300

    contexts, first version, not yet stitched with terminals

---
+++ base_system/context/cxt_majority_large_ships.py
@@ -0,0 +1,16 @@
+
+from base_system.context.abstract_context import AbstractContext
+from base_system.ship import Ship
+import random
+
+class MajorityLargeShips (AbstractContext):
+    def __init__(self):
+        super().__init__()
+        self.context_name = "Majority of the ships are small"
+        self.ships_arrived = []
+        self.ship_unique_id = 1
+
+    def step(self):
+        if random.choice([True, False]): #faster solution bool(random.getrandbits(1))
+            self.ships_arrived.append(Ship(self.ship_unique_id))
+            self.ship_unique_id += 1
