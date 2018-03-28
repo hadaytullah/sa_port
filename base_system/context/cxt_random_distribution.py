814bf3ca98c81a87d056c9f056885522d9ac2799
Simo Linkola <slinkola@cs.helsinki.fi>
2018-03-28 08:02:04 +0300

    Changed RandomBistribution context to BernoulliDistribution with a parameter p for wider usage.

--- base_system/context/cxt_random_distribution.py
+++ base_system/context/cxt_random_distribution.py
@@ -4,15 +4,18 @@ from base_system.context.abstract_context import AbstractContext
 from base_system.ship import Ship
 
 
-class RandomDistribution (AbstractContext):
-    def __init__(self):
+class BernoulliDistribution (AbstractContext):
+    """Context creating ships using Bernoulli distribution with given p.
+    """
+    def __init__(self, p=0.5):
         super().__init__()
-        self.context_name = "Majority of the ships are small"
+        self.context_name = "CTX Bernoulli distribution"
+        self.p = p
         self.ships_arrived = []
         self.ship_unique_id = 1
 
     def step(self, **kwargs):
-        if random.choice([True, False]): #faster solution bool(random.getrandbits(1))
+        if random.random() < self.p:
             self.ships_arrived.append(Ship(self.ship_unique_id))
             self.ship_unique_id += 1
 
